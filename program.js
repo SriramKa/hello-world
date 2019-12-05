7ab8bf35b9e84dd3c776e79677311274e56e624d
SriramKa <fan4aram@gmail.com>
2019-12-05 13:27:49 +0530

    random additons to program.js

--- program.js
+++ program.js
@@ -3,8 +3,14 @@ function main()
     var hello = 'Hello, World!'
     console.log(hello)
 
-    var h = ['a','b','c']
-    console.log(h)
+    var h = {
+        name: 'Hello',
+        say: function () {
+            console.log(this.name)
+        }
+    }
+    
+    h.say()
 }
 
 //test comment
